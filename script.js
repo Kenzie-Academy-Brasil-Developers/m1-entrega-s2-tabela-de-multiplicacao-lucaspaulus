88bfe1ad464d50dee39217ca791eb1b87db84ed5
lucas <lucas.apaulus012@gmail.com>
2021-11-23 11:05:01 -0300

    Criei uma Tabela de multiplicaçao

---
+++ script.js
@@ -0,0 +1,13 @@
+function nomeDaSuaFuncao() { // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
+    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
+    let multi = [];
+    for (let row = 0; row <= 10; row++) {
+        multi[row] = []
+        for(let column = 0; column <= 10; column++){
+            multi[row][column] = row * column
+        }
+    }
+    return multi
+}
+
+console.table(nomeDaSuaFuncao())
